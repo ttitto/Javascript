c62f112df4511aa1412f76360b20a68e07a9f126
ttitto <ttitto@users.noreply.github.com>
2014-07-15 00:48:26 +0300

    Tic Tac Toe game finished

--- JSIntroductionHW/Task06TicTacToe/scripts/Task06TicTacToe.js
+++ JSIntroductionHW/Task06TicTacToe/scripts/Task06TicTacToe.js
@@ -1,15 +1,71 @@
-function onClickHandler(event){
-    var eTarget=event.target || event.srcElement;
+var sign = "cross";// sets initial sign value
+var patternX;
+var patternY;
+window.onload = onLoadHandler;
 
-    alert(eTarget.id);
-    function getClickedId(){
-        return eTarget.id;
+function onClickHandler(event) {
+    var eTarget = event.target || event.srcElement;
+    var el = eTarget;
+    if (!el.classList.contains("cross") && !el.classList.contains("circle")) {
+        el.className += " " + sign
+
+        if (sign == "cross") {
+            patternX[parseInt(el.id)] = 1;
+        }
+        if (sign == "circle") {
+            patternY[parseInt(el.id)] = 1;
+        }
+
+        if (CheckWin(patternX) || CheckWin(patternY)) {
+            document.getElementsByClassName("game-over-message").item(0).style.display = "block";
+            document.getElementById("game").style.opacity = "0.15";
+        }
+
+        toggleSign();
+    }
+
+}
+function CheckWin(pattern) {
+
+    if ((pattern[0] == 1 && pattern[1] == 1 && pattern[2] == 1) ||
+        (pattern[3] == 1 && pattern[4] == 1 && pattern[5] == 1) ||
+        (pattern[6] == 1 && pattern[7] == 1 && pattern[8] == 1) ||
+        (pattern[0] == 1 && pattern[3] == 1 && pattern[6] == 1) ||
+        (pattern[1] == 1 && pattern[4] == 1 && pattern[7] == 1) ||
+        (pattern[2] == 1 && pattern[5] == 1 && pattern[8] == 1) ||
+        (pattern[0] == 1 && pattern[4] == 1 && pattern[8] == 1) ||
+        (pattern[2] == 1 && pattern[4] == 1 && pattern[6] == 1)) {
+        return true;
+    } else {
+        return false;
     }
 }
-function onLoadHandler(event){
-    document.getElementsByName('div').onclick=onClickHandler(event);
+
+function toggleSign() {
+    if (this.sign == "cross") {
+        this.sign = "circle";
+    } else {
+        this.sign = "cross";
+    }
 }
 
-function a (){
-    window.onload=onLoadHandler;
+function onLoadHandler() {
+    //attach onclick events on the cells
+    var elements = document.getElementsByClassName("cell");
+
+    for (var i = 0; i < elements.length; i++) {
+        var el = elements.item(i);
+        if (el.addEventListener) {
+            el.addEventListener("click", onClickHandler, false);
+        }
+        else {
+            el.attachEvent("onclick", onClickHandler)
+        }
+    }
+
+    //Initialize pattern arrays
+    this.patternX = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    this.patternY = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 }
+
