ec533600b61a87817ceca312b8ea2a5d835efd5a
ttitto <ttitto@users.noreply.github.com>
2014-07-18 20:00:12 +0300

    where is the problem?

--- JSLoopsArraysStringsHW/Task08SelectionSortArray/arraySorter.js
+++ JSLoopsArraysStringsHW/Task08SelectionSortArray/arraySorter.js
@@ -1,26 +1,29 @@
 //SELECTION SORT ALGORITHM
 function sortArray(value) {
-    var i;
-    var smIndex = 0;
-    var smNum = value[0];
     var resultArr = [];
+    var len = value.length;
     //loop through all elements and find smallest number and its index
-    for (i = 1; i < value.length; i++) {
-        if (value[i] < value[i - 1]) {
-            smNum = value[i];
-            smIndex = i;
-        }
-        if (i == value.length - 1) {
-            //add smallest index at the end of a new array
-            resultArr.push(smNum);
-            //remove the smallest number from the initial array
-            value.splice(smIndex, 1);
-            i = 0;
-            smNum = value[0];
-            smIndex = 0;
-        }
+    for (var i = 0; i < len; i++) {
+        var smallest = getSmallest(value);
+        //add smallest index at the end of a new array
+        resultArr.push(smallest[0]);
+        //remove the smallest number from the initial array
+        value.splice(smallest[1], 1);
     }
     return resultArr.join(' ');
 }
-console.log(sortArray([5, 4, 3, 1, 2, 1]));
+
+function getSmallest(arr) {
+    var len = arr.length;
+    var result = [arr[0], 0];
+    for (var i = 1; i < len; i++) {
+        if (arr[i] < arr[i - 1]) {
+            result[0] = arr[i];
+            result[1] = i;
+        }
+    }
+    return result;
+}
+
 console.log(sortArray([12, 12, 50, 6, 2, 22, 51, 712, 6, 3, 3]));
+console.log(sortArray([5, 4, 3, 1, 2, 12]));
