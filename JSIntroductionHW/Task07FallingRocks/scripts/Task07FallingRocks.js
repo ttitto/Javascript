1245c6e884103b0e48f5a6deca8ea86775acaf05
ttitto <ttitto@users.noreply.github.com>
2014-07-20 17:12:41 +0300

    falling rock slightly forwarded

--- JSIntroductionHW/Task07FallingRocks/scripts/Task07FallingRocks.js
+++ JSIntroductionHW/Task07FallingRocks/scripts/Task07FallingRocks.js
@@ -1,46 +1,17 @@
-/*ALGORITHM
-*
-*   change the windows limits on window.resize
-*   CREATE RANDOM SITUATED particles - have initial coordinates and movement direction
-*
-* */
-var fallingElements=new Array(10);
-var gameField=document.getElementById("game-field");
-window.onload=onLoadHandler;
+var fallingElements = new Array(10);
 
-function onLoadHandler(){
-    //attach window onresize event handler
-//addOnresizeEvent(window,"resize",onWindowResizeHandler);
-    gameRun();
-}
+window.onload = onLoadHandler;
 
-function gameRun(){
-    generateSuns(window.innerWidth);
-    drawSuns();
+function onLoadHandler() {
+    gameField = document.getElementById("game-field");
+
+    createFallingElement();
 
 }
-
-function generateSuns(posMax){
-    var posX=Math.floor((Math.random() * posMax) + 1);
-    fallingElements.add([0,posX]);
-
-}
-function drawSuns(){
-    var el=document.createElement("div");
-    el.className="sun";
-
+function createFallingElement() {
+    var el = document.createElement('div');
+    el.className = 'shit';
+    el.style.left = 720 + 'px';
+    el.style.top = -20 +'px';
     gameField.appendChild(el);
 }
-//function onWindowResizeHandler(){
-//    //set the allowed max position of the falling blocks
-//}
-//function addOnresizeEvent(elem, type, eventHandle) {
-//    if (elem == null || typeof(elem) == 'undefined') return;
-//    if ( elem.addEventListener ) {
-//        elem.addEventListener( type, eventHandle, false );
-//    } else if ( elem.attachEvent ) {
-//        elem.attachEvent( "on" + type, eventHandle );
-//    } else {
-//        elem["on"+type]=eventHandle;
-//    }
-//};
